a141ca768e3c62b05e632f4a8b1ca0d77c88bb4a
Amyr <44586350+ChelokovAmyr@users.noreply.github.com>
2022-04-12 10:28:13 +0300

    new Files

---
+++ src/components/IncomeList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="income-list">
+        <IncomeItem v-for="obj in state.sortedIncome" :key="obj.id" :income="obj" @remove-item="removeItem"/>
+    </div>
+</template>
+
+<script>
+import IncomeItem from './IncomeItem'
+export default {
+    props: {
+        state: Object
+    },
+    setup(props, {emit}){
+        function removeItem(id){
+            emit('remove-item', id)
+        }
+
+        return {
+            IncomeItem,
+             removeItem
+        }
+    }
+}
+</script>
